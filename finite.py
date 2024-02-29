afbfd14138110d939d2593e1bfb07a80ff3b1a17
Bobreyshuffles <you@example.com>
2024-02-28 19:06:38 -0600

    finished

--- finite.py
+++ finite.py
@@ -2,19 +2,36 @@ from llist import LList, Node, append
 
 
 def length(lst):
-    """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
-
+    if lst.head:
+        node=lst.head
+        counter=1 
+        while node.next:
+            counter+= 1
+            node=node.next
+    else:
+        counter=0
+    print(counter)
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
+    if lst.head:  # same as:  if lst.head is not None
+        node = lst.head
+        while node.next:
+            print(node.val, end=" ")
+            node=node.next
+        print(node.val)
+    else:
+        print("empty")
+
 
 
 if __name__ == "__main__":
-
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+    nums= [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    lst= LList()
+    for i in nums:
+        append(lst,Node(i))
+    llprint(lst)
+    length(lst)
+        
+from genfinite import lst 
+print(length(lst))
